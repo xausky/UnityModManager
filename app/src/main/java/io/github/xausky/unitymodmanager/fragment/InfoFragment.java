f7b1ba97f9bcf521eec8b211a92cd14a6de342fd
xausky <xausky@163.com>
2018-04-03 18:34:23 +0800

    RC2发布，修改关于文本

--- app/src/main/java/io/github/xausky/unitymodmanager/fragment/InfoFragment.java
+++ app/src/main/java/io/github/xausky/unitymodmanager/fragment/InfoFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 
 import io.github.xausky.unitymodmanager.R;
 import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 /**
  * Created by xausky on 18-3-3.
@@ -19,9 +20,9 @@ public class InfoFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return new AboutPage(inflater.getContext())
                 .setDescription(getString(R.string.info_context_text))
-                .addEmail("xausky@163.com")
+                .addEmail("xausky@163.com", "我的邮箱")
                 .addWebsite("https://space.bilibili.com/8419077/", "我的B站")
-                .addGitHub("xausky")
+                .addGitHub("xausky", "我的Github")
                 .create();
     }
 }
